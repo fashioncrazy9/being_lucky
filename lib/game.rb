2795d88d44d05d39ee26136e5fae1e5cb4fd4fcd
Ellis Gray <ellisgray2012@gmail.com>
2015-09-30 23:43:43 +1000

    clean up

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,18 @@
-require 'die'
+#!/usr/bin/env ruby
+require_relative 'being_lucky'
+require 'forwardable'
+require_relative 'game/die'
+require_relative 'game/roll'
+require_relative 'game/calculator'
+
+run = true
+while (run)
+  puts 'How many dice are you rolling?'
+  dice = gets.to_i
+  puts "Your score is: #{BeingLucky.new(dice).score}"
+  puts "Roll again? (Y/n) ?"
+  again = gets
+  if again.include?('N') || again.include?('n')
+    run = false
+  end
+end
